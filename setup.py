93d3414b1537fc00ecc3cc29ed06287defd5d0ce
Sayantan Das <santaclaus30011998@gmail.com>
2023-05-20 00:40:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='bongovaad',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=requirements,
 )
